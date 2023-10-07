39ebe93d4153dd346029ab48a02454b0bd606a75
Guillaume <guillaume.grataloup@etu.unice.fr>
2023-10-07 22:50:25 +0200

    functionalised clash reaserch

--- New_Code/Chimer.py
+++ New_Code/Chimer.py
@@ -6,8 +6,12 @@ from chimera.specifier import evalSpec
 from Rotamers import getRotamers
 import os
 
-newAA = "TYR"
+newAA = "ALA"
 posAA = "673"
+pdb_file = "C:/Users/Guillaume/Desktop/hERG/New_Code/hERG.pdb"
+ROTA_PROB_THRESHOLD = 0.1
+MIN_NUM_ROTA = 3
+
 
 ## get the list of residues in the 5 angstom zone around selected residue
 def zone(res):
@@ -34,49 +38,71 @@ def clashes(file,residue_index):
             nb_of_clashes -= 1
     return nb_of_clashes
 
-## we open chimera for the first time to get the rotamers and their probabilities of the mutated AA
-chimera.openModels.open("C:/Users/Guillaume/Desktop/hERG/New_Code/hERG.pdb")
+## get probability of rotamers of amino acid "AA" at position "pos"
+def get_rota(AA,pos):
+    rc("swapaa "+AA+" :"+pos+".a")
+    r = evalSpec(" :"+pos+".a").residues()[0]
+    try : 
+        (flag,rotamers) = getRotamers(r)
+    except :
+        return r,[]
+    proba_rotamer = []
+    for i in range(len(rotamers)):
+        proba_rotamer.append(rotamers[i].rotamerProb)
+    return r,proba_rotamer
+    
+def clash(AA,pos,rot):
+    #chimera.openModels.open(pdb_file)
+    rc("swapaa "+AA+" :"+pos+".a criteria "+rot)
+    rc("addh spec sel")
+    rc("select :"+pos+".a za<5")
+    rc("findclash sel test self ignoreIntraRes true colorClashes true clashColor red saveFile clashes{}.txt namingStyle simple summary true log true".format(rot))
+
+
+
 f = open("output.txt","w")
-rc("swapaa "+newAA+" :"+posAA+".a") #mutate the residue
-r = evalSpec(" :"+posAA+".a").residues()[0] #get the AA object at the position
+## we open chimera for the first time to get the rotamers and their probabilities of the mutated AA
+chimera.openModels.open(pdb_file)
+r,proba_rotamer = get_rota(newAA,posAA)
+
+
+## log
+
+f.write("residue : \n")
 f.write(str(r)+"\n")
-rotamers = getRotamers(r) #get the rotamers of the AA
-proba_rotamer = []
-for i in range(len(rotamers[1])): #get the probability of the rotamers
-    proba_rotamer.append(rotamers[1][i].rotamerProb) 
+f.write("proba rota : \n")
 f.write(str(proba_rotamer)+"\n")
-rc("close session")
-#if a proba of a rotamer is >= 10% it's considered,
-#if no rotamers have a probability above 10%, we consider only the 3 firsts
+
+#if a proba of a rotamer is >= ROTA_PROB_THRESHOLD % it's considered,
+#if no rotamers have a probability above ROTA_PROB_THRESHOLD %, we consider only the MIN_NUM_ROTA firsts
 nb_of_interest_rot = 0
 for e in proba_rotamer :
-    if e >= 0.1:
+    if e >= ROTA_PROB_THRESHOLD:
         nb_of_interest_rot += 1
 if not nb_of_interest_rot:
-    nb_of_interest_rot = 3
+    nb_of_interest_rot = MIN_NUM_ROTA
 f.write(str(nb_of_interest_rot)+"\n")
+
 
 # get the numbers of clashes for each intersting rotamers, minimize the structure for each, and get the new number of clashes
 clashes_of_rota = []
 clashes_of_rota_after = []
+
+rc("close session")
+chimera.openModels.open(pdb_file)
 for i in range(nb_of_interest_rot):
-    chimera.openModels.open("C:/Users/Guillaume/Desktop/hERG/New_Code/hERG.pdb")
-    rc("swapaa "+newAA+" :"+posAA+".a criteria {}".format(i+1))
-    rc("addh spec sel")
-    rc("select :"+posAA+".a za<5")
-    rc("findclash sel test self ignoreIntraRes true colorClashes true clashColor red saveFile clashes{}.txt namingStyle simple summary true log true".format(i+1))
+    clash(newAA,posAA,str(i+1))
     clashes_of_rota.append(clashes("clashes{}.txt".format(i+1),posAA))
-    rc("close session")
 f.write(str(clashes_of_rota)+"\n")
 
+rc("close session")
 for i in range(nb_of_interest_rot):
     if clashes_of_rota[i]:
-        chimera.openModels.open("C:/Users/Guillaume/Desktop/hERG/New_Code/hERG.pdb")
+        chimera.openModels.open(pdb_file)
         rc("select :"+posAA+".a za<5")
         rc("minimize spec sel nogui True nsteps 10 cgsteps 0 ")
         rc("findclash sel test self ignoreIntraRes true colorClashes true clashColor red saveFile clashes{}.txt namingStyle simple summary true log true".format(i+1))
         clashes_of_rota_after.append(clashes("clashes{}.txt".format(i+1),posAA))
-        rc("close session")
 
 f.write(str(clashes_of_rota_after))
 
@@ -85,7 +111,7 @@ f.close()
 
 
 
-#test
+
 
 
 
